8074c6222ff963e5d55c4b1b8bae06eaeae44534
Don Morgan <don.morgan@gov.bc.ca>
2019-01-21 15:05:54 -0800

    Initial commit

---
+++ 03_analysis.R
@@ -0,0 +1,17 @@
+# Copyright 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+source("header.R")
+
+Mort_UnRep$Reported_Unknown <- (Mort_UnRep$ReportedFemaleMort+Mort_UnRep$ReportedFemaleMort*Mort_UnRep$UnReport)
+
+Mort_UnRep$pc_Mort <- round(Mort_UnRep$Reported_Unknown/Mort_UnRep$PopEst*100,2)
